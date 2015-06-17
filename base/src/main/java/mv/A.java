c5280e35b163e2a952decdb8075eba5115c05bd0
Hervé Boutemy <hboutemy@apache.org>
2015-06-17 18:34:39 +0200

    added TODO: compiling this class should fail, since expected to be JDK7 compliant

--- base/src/main/java/mv/A.java
+++ base/src/main/java/mv/A.java
@@ -1,6 +1,6 @@
 package mv;
 
-import java.util.stream.Stream;
+import java.util.stream.Stream; // JDK 8: compiling with JDK 7 should fail (either using toolchains or AnimalSniffer)
 
 public class A {
     Stream<String> s = null;
