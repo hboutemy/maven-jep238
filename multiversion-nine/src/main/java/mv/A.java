504b28d513425f86e59fc80dc8780485c1293264
Paul Sandoz <paul.d.sandoz@googlemail.com>
2015-06-22 14:50:30 +0200

    Use java 9 ea-specific language and library features

--- multiversion-nine/src/main/java/mv/A.java
+++ multiversion-nine/src/main/java/mv/A.java
@@ -2,11 +2,23 @@ package mv;
 
 import base.Base;
 
+import java.util.Optional;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class A {
     public static String getString() {
-        Supplier<String> s = () -> "NINE";
+        Optional<String> o = Optional.of("NINE");
+        // The method Optional.stream only exists in Java 9 ea builds
+        Stream<String> os = o.stream();
+
+        String m;
+        // New Java 9 ea language feature for try-with-resources
+        try (os) {
+            m = os.findFirst().get();
+        }
+
+        Supplier<String> s = () -> m;
         return Base.get() + " -> " + s.get();
     }
 }
